d3f6afb08f2422e9fc7ce900f5032a45392ff79e
seola1ne <seolalny@gmail.com>
2024-05-24 09:27:02 +0900

    add : color, font 추가

---
+++ src/styles/font.jsx
@@ -0,0 +1,35 @@
+import { css } from "styled-components";
+
+const fontGenerator = (
+	weight,
+	size,
+    color
+) => css`
+	font-family: "Pretendard";
+	font-weight: ${weight};
+	font-size: ${size}rem;
+	letter-spacing: -0.5px;
+    color: ${color};
+`;
+
+const font = {
+	H1: fontGenerator(600, 1.125, "#000000"),
+	H2: fontGenerator(600, 1, "#000000"),
+
+    SubTitle: fontGenerator(400, 1, "#8292AA"),
+
+	p1: fontGenerator(400, 1, "#000000"),
+	p2: fontGenerator(400, 0.875, "#000000"),
+	p3: fontGenerator(400, 0.75, "#000000"),
+
+    p1_bold: fontGenerator(600, 1, "#000000"),
+    p2_bold: fontGenerator(600, 0.875, "#000000"),
+    p3_bold: fontGenerator(600, 0.75, "#000000"),
+    
+    button: fontGenerator(400, 0.875, "#FFFFFF"),
+    caption: fontGenerator(400, 0.813, "#000000"),
+    placeholder: fontGenerator(300, 0.75, "#A8B3C4"),
+    navigator: fontGenerator(600, 0.75, "#A8B3C4"),
+};
+
+export default font;

f37737c4ee892d0f001e80778f661abb773440f2
seola1ne <seolalny@gmail.com>
2024-06-26 10:00:22 +0900

    chore : userData ID 수정

--- frontend/src/data/userData.js
+++ frontend/src/data/userData.js
@@ -6,20 +6,20 @@ const userData = [
         birthday: "20060915"
     },
     {
-        id: 2,
-        name: "a123",
+        id: "a123",
+        name: "김철수",
         password: "abcd1234#",
         birthday: "19851120"
     },
     {
-        id: 3,
-        name: "b123",
+        id: "b123",
+        name: "이영희",
         password: "xyz56789$",
         birthday: "19920801"
     },
     {
-        id: 4,
-        name: "c123",
+        id: "c123",
+        name: "홍길동",
         password: "qwer1234%",
         birthday: "19880310"
     }
--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -14,7 +14,7 @@ import Button from 'components/Common/Button';
 import Row from 'components/Common/Flex/Row';
 
 function Profile() {
-    const userId = "seola1ne";
+    const userId = "a123";
     const user = userData.find(u => u.id === userId);
     const keywordInfo = keywordData.find(data => data.userId === user.id);
 
